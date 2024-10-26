824e2ea9d5af2fc4dc339eaca843b32621f1f97a
codytseng <codytseng98@gmail.com>
2024-10-26 18:10:41 +0800

    🎉

---
+++ README.md
@@ -0,0 +1,35 @@
+# jumble
+
+Yet another Nostr desktop client
+
+## Build from source
+
+You can also build the app from the source code.
+
+> Note: Node.js >= 20 is required.
+
+```bash
+# Clone this repository
+git clone https://github.com/CodyTseng/jumble.git
+
+# Go into the repository
+cd jumble
+
+# Install dependencies
+npm install
+
+# Build the app
+npm run build:mac
+# or npm run build:win
+# or npm run build:linux
+```
+
+The executable file will be in the `dist` folder.
+
+## Donate
+
+If you like this project, you can buy me a coffee :) ⚡️ codytseng@getalby.com ⚡️
+
+## License
+
+MIT
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/renderer/**/*.{ts,tsx}'],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        muted: 'hsl(var(--muted))',
+        'muted-foreground': 'hsl(var(--muted-foreground))',
+        card: 'hsl(var(--card))',
+        'card-foreground': 'hsl(var(--card-foreground))',
+        popover: 'hsl(var(--popover))',
+        'popover-foreground': 'hsl(var(--popover-foreground))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        primary: 'hsl(var(--primary))',
+        'primary-foreground': 'hsl(var(--primary-foreground))',
+        secondary: 'hsl(var(--secondary))',
+        'secondary-foreground': 'hsl(var(--secondary-foreground))',
+        accent: 'hsl(var(--accent))',
+        'accent-foreground': 'hsl(var(--accent-foreground))',
+        destructive: 'hsl(var(--destructive))',
+        'destructive-foreground': 'hsl(var(--destructive-foreground))',
+        ring: 'hsl(var(--ring))',
+        highlight: 'hsl(var(--highlight))'
+      }
+    }
+  },
+  plugins: [require('tailwindcss-animate')]
+}
