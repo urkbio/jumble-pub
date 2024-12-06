cefe3bb66aad2feb6d57cc1e336860251430499c
codytseng <codytseng@outlook.com>
2024-12-06 17:48:25 +0800

    fix: correct logo width issue in Safari

--- src/renderer/src/assets/Logo.tsx
+++ src/renderer/src/assets/Logo.tsx
@@ -1,8 +1,6 @@
 export default function Logo({ className }: { className?: string }) {
   return (
     <svg
-      width="100%"
-      height="100%"
       viewBox="0 0 2154 559"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
