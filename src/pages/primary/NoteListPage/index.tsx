f75f32b01866fb32aa0d5e3f247b9dfba181292b
cxplay <62034099+cxplay@users.noreply.github.com>
2025-04-09 11:54:55 +0800

    修复社区中继列表

--- src/pages/primary/NoteListPage/index.tsx
+++ src/pages/primary/NoteListPage/index.tsx
@@ -1,78 +1,42 @@
 import NoteList from '@/components/NoteList'
 import PostEditor from '@/components/PostEditor'
-import SaveRelayDropdownMenu from '@/components/SaveRelayDropdownMenu'
 import { Button } from '@/components/ui/button'
 import PrimaryPageLayout from '@/layouts/PrimaryPageLayout'
-import { useFeed } from '@/providers/FeedProvider'
 import { useNostr } from '@/providers/NostrProvider'
 import { useScreenSize } from '@/providers/ScreenSizeProvider'
 import { TPageRef } from '@/types'
-import { PencilLine } from 'lucide-react'
-import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import FeedButton from './FeedButton'
+import { PencilLine, Server } from 'lucide-react'
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 import SearchButton from './SearchButton'
 
 const NoteListPage = forwardRef((_, ref) => {
-  const { t } = useTranslation()
   const layoutRef = useRef<TPageRef>(null)
-  const { pubkey, checkLogin } = useNostr()
-  const { feedInfo, relayUrls, isReady, filter } = useFeed()
   useImperativeHandle(ref, () => layoutRef.current)
-
-  useEffect(() => {
-    if (layoutRef.current) {
-      layoutRef.current.scrollToTop()
-    }
-  }, [JSON.stringify(relayUrls), feedInfo])
-
-  let content = <div className="text-center text-sm text-muted-foreground">{t('loading...')}</div>
-  if (feedInfo.feedType === 'following' && !pubkey) {
-    content = (
-      <div className="flex justify-center w-full">
-        <Button size="lg" onClick={() => checkLogin()}>
-          {t('Please login to view following feed')}
-        </Button>
-      </div>
-    )
-  } else if (isReady) {
-    content = (
-      <NoteList
-        relayUrls={relayUrls}
-        filter={filter}
-        needCheckAlgoRelay={feedInfo.feedType !== 'following'}
-      />
-    )
-  }
 
   return (
     <PrimaryPageLayout
       pageName="home"
       ref={layoutRef}
-      titlebar={
-        <NoteListPageTitlebar
-          temporaryRelayUrls={feedInfo.feedType === 'temporary' ? relayUrls : []}
-        />
-      }
+      titlebar={<NoteListPageTitlebar />}
       displayScrollToTopButton
     >
-      {content}
+      <NoteList relayUrls={['wss://relay.nostr.moe/', 'wss://relay.cxplay.org/']} />
     </PrimaryPageLayout>
   )
 })
 NoteListPage.displayName = 'NoteListPage'
 export default NoteListPage
 
-function NoteListPageTitlebar({ temporaryRelayUrls = [] }: { temporaryRelayUrls?: string[] }) {
+function NoteListPageTitlebar() {
   const { isSmallScreen } = useScreenSize()
 
   return (
     <div className="flex gap-1 items-center h-full justify-between">
-      <FeedButton className="flex-1 max-w-fit w-0" />
+      <div className="flex gap-2 items-center h-full pl-3">
+        <Server />
+        <div className="text-lg font-semibold">Nostr.moe</div>
+      </div>
       <div className="shrink-0 flex gap-1 items-center">
-        {temporaryRelayUrls.length > 0 && (
-          <SaveRelayDropdownMenu urls={temporaryRelayUrls} atTitlebar />
-        )}
         <SearchButton />
         {isSmallScreen && <PostButton />}
       </div>
