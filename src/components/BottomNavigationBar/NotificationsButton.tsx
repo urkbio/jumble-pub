8712b59ae54e56ec6f614a07d7dbe42d1aa1b571
codytseng <codytseng98@gmail.com>
2025-04-12 17:47:12 +0800

    feat: show login modal when clicking notification without being logged in

--- src/components/BottomNavigationBar/NotificationsButton.tsx
+++ src/components/BottomNavigationBar/NotificationsButton.tsx
@@ -1,16 +1,18 @@
 import { usePrimaryPage } from '@/PageManager'
+import { useNostr } from '@/providers/NostrProvider'
 import { useNotification } from '@/providers/NotificationProvider'
 import { Bell } from 'lucide-react'
 import BottomNavigationBarItem from './BottomNavigationBarItem'
 
 export default function NotificationsButton() {
+  const { checkLogin } = useNostr()
   const { navigate, current } = usePrimaryPage()
   const { hasNewNotification } = useNotification()
 
   return (
     <BottomNavigationBarItem
       active={current === 'notifications'}
-      onClick={() => navigate('notifications')}
+      onClick={() => checkLogin(() => navigate('notifications'))}
     >
       <div className="relative">
         <Bell />
--- src/components/Sidebar/NotificationButton.tsx
+++ src/components/Sidebar/NotificationButton.tsx
@@ -1,16 +1,18 @@
 import { usePrimaryPage } from '@/PageManager'
+import { useNostr } from '@/providers/NostrProvider'
 import { useNotification } from '@/providers/NotificationProvider'
 import { Bell } from 'lucide-react'
 import SidebarItem from './SidebarItem'
 
 export default function NotificationsButton() {
+  const { checkLogin } = useNostr()
   const { navigate, current } = usePrimaryPage()
   const { hasNewNotification } = useNotification()
 
   return (
     <SidebarItem
       title="Notifications"
-      onClick={() => navigate('notifications')}
+      onClick={() => checkLogin(() => navigate('notifications'))}
       active={current === 'notifications'}
     >
       <div className="relative">
