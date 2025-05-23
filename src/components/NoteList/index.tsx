6ee9cc1fd282ee03ae07e52b39649e4c1f673442
codytseng <codytseng@outlook.com>
2025-05-23 09:58:04 +0800

    feat: add highlights to feed

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -110,7 +110,7 @@ export default function NoteList({
         subRequests.push({
           urls: myRelayList.write.concat(BIG_RELAY_URLS).slice(0, 5),
           filter: {
-            kinds: [kinds.ShortTextNote, kinds.Repost],
+            kinds: [kinds.ShortTextNote, kinds.Repost, kinds.Highlights],
             authors: [pubkey],
             '#p': [author],
             limit: LIMIT
@@ -119,7 +119,7 @@ export default function NoteList({
         subRequests.push({
           urls: targetRelayList.write.concat(BIG_RELAY_URLS).slice(0, 5),
           filter: {
-            kinds: [kinds.ShortTextNote, kinds.Repost],
+            kinds: [kinds.ShortTextNote, kinds.Repost, kinds.Highlights],
             authors: [author],
             '#p': [pubkey],
             limit: LIMIT
@@ -135,7 +135,7 @@ export default function NoteList({
           kinds:
             filterType === 'pictures'
               ? [ExtendedKind.PICTURE]
-              : [kinds.ShortTextNote, kinds.Repost],
+              : [kinds.ShortTextNote, kinds.Repost, kinds.Highlights],
           limit: areAlgoRelays ? ALGO_LIMIT : LIMIT
         }
         if (relayUrls.length === 0 && (_filter.authors?.length || author)) {
